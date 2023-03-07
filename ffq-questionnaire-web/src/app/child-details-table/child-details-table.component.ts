29ceb2fa65787b535df803ca038b6109e3b98b63
ChristianDerisca <ChristianDeriscaa@gmail.com>
2023-03-06 23:33:14 -0500

    Hard coding History

--- ffq-questionnaire-web/src/app/child-details-table/child-details-table.component.ts
+++ ffq-questionnaire-web/src/app/child-details-table/child-details-table.component.ts
@@ -1,5 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DatePipe } from '@angular/common';
+
+import { MatDialog } from "@angular/material/dialog";
+import { Observable } from "rxjs";
+import { TranslateService } from "@ngx-translate/core";
+import { ThemePalette } from "@angular/material/core";
+import { ProgressSpinnerMode } from "@angular/material/progress-spinner";
+import html2canvas from "html2canvas";
+
+import { ParentService } from "src/app/services/parent/parent-service";
+import { AuthenticationService } from "../services/authentication/authentication.service";
+import { FFQParentResponse } from "src/app/models/ffqparent-response";
+
+import { FFQChildren } from "src/app/models/ffqchildren";
+import { FFQChildData } from "src/app/models/ffq-childData";
+
+import {
+  UnitsOfMeasurement,
+  Gender,
+  ChartOption,
+  GrowthChartData,
+} from "src/app/models/Enums";
 
 @Component({
   selector: 'app-child-details-table',
@@ -8,16 +29,16 @@ import { DatePipe } from '@angular/common';
 })
 export class ChildDetailsTableComponent implements OnInit {
   
-  people: {name: string; length: string; weight: string; age: number; }[];
+  people: {date: string; name: string; length: string; weight: string; age: number; }[];
 
   constructor() { }
 
   ngOnInit(): void {
     this.people = [
-      { name: 'John', length: '180cm', weight: '75kg', age: 24 },
-      { name: 'Jane', length: '165cm', weight: '60kg', age: 30 },
-      { name: 'Bob', length: '190cm', weight: '90kg', age: 45 },
-      { name: 'Alice', length: '155cm', weight: '50kg', age: 18 },
+      {date:'January 10th, 2022', name: 'John', length: '65.9', weight: '7.4', age: 5 },
+      {date:'June 10th, 2022', name: 'John', length: '73.3', weight: '9.7', age: 10 },
+      {date:'November 10th, 2022', name: 'John', length: '75', weight: '11', age: 15 },
+      {date:'April 10th, 2024', name: 'John', length: '80', weight: '14kg', age: 20 },
     ];
   }
 
